1a8efd6a6e1ba0aefd9919c9bd6dc1fd23fd6b2c
WaifuRain <Xcalibur996@gmail.com>
2020-07-15 07:47:44 -0400

    test file

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-from get_attributes import Character
+# from get_attributes import Character
+#
+# test_waifu = Character(533)
+# print(test_waifu.first)
+# print(test_waifu.description)
 
-test_waifu = Character(533)
-print(test_waifu.first)
+from MAL_Parser import Character
+
+test_waifu = Character(1)
+print(test_waifu.name)
+print(test_waifu.kanji)
 print(test_waifu.description)
+print(test_waifu.actors)
