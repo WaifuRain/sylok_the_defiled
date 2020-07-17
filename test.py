475436bc1c291fec1c79a704545266ff6e495aba
WaifuRain <Xcalibur996@gmail.com>
2020-07-17 07:32:51 -0400

    test file

--- test.py
+++ test.py
@@ -5,9 +5,18 @@
 # print(test_waifu.description)
 
 from MAL_Parser import Character
+from waifu_db import waifu_db
 
-test_waifu = Character(1)
-print(test_waifu.name)
-print(test_waifu.kanji)
-print(test_waifu.description)
-print(test_waifu.actors)
+test_waifu = Character(249)
+# print(test_waifu.name)
+# print(test_waifu.kanji)
+# print(test_waifu.description)
+# print(test_waifu.actors)
+# print(test_waifu.animeography)
+# print(test_waifu.mangaography)
+print(test_waifu.images)
+# print(test_waifu.initials)
+# print(test_waifu.first)
+# print(test_waifu.last)
+
+# print(waifu_db.keys())
