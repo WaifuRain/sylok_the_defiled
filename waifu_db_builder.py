2479297f6d3b5a899f69316beba7940526a25310
WaifuRain <Xcalibur996@gmail.com>
2020-07-17 07:34:12 -0400

    updated and organized db builder

--- waifu_db_builder.py
+++ waifu_db_builder.py
@@ -1,11 +1,6 @@
-from get_attributes import Character
+from MAL_Parser import Character
 import json
 import id_list as id_lst
-from waifu_db import waifu_db
-"""
-Check the name of id:620 in the waifu database
-
-"""
 
 
 def count_lines(file):
@@ -17,17 +12,32 @@ def count_lines(file):
     return lines
 
 
+with open('waifu_db.py', 'r') as check:
+    if check.read() == '':
+        with open('waifu_db.py', 'w') as waifu_db:
+            waifu_db.write('waifu_db = {}\n')
+
 with open('valid_id_list_new.txt', 'r') as id_list:
     character_id_list = []
     for i in range(count_lines('valid_id_list_new.txt')):
         id_string = id_list.readline().strip()
         character_id_list.append(id_string[id_string.find(":")+1:])
 
+from waifu_db import waifu_db
 new_dictionary = waifu_db
 for waifu_id in character_id_list:
-    w = Character(waifu_id)
-    new_dictionary.update({w.character_id: {'name': w.name, 'description': w.description, 'images': w.images}})
+    if waifu_id in waifu_db.keys():
+        pass
+    else:
+        w = Character(waifu_id)
+        new_dictionary.update({w.character_id: {
+            'info': {
+                'name': w.name, 'kanji': w.kanji, 'initials': w.initials, 'description': w.description},
+                'images': w.images,
+                'appearances': {'animeography': w.animeography, 'mangaography': w.mangaography},
+                'actors': w.actors}
+            })
 
-    with open('waifu_db.py', 'w') as db:
-        db.write(f'waifu_db = {json.dumps(new_dictionary, indent=2)}')
-        print(f"Waifu '{w.name}' written to database.")
+        with open('waifu_db.py', 'w') as db:
+            db.write(f'waifu_db = {json.dumps(new_dictionary, indent=4)}')
+            print(f"Waifu '{w.name}' written to database.")
