60f6c6ab5956b1f96bf43d9ab0fc18e5cf3c20e1
WaifuRain <Xcalibur996@gmail.com>
2020-07-18 01:34:16 -0400

    test file changes

--- test.py
+++ test.py
@@ -7,16 +7,7 @@
 from MAL_Parser import Character
 from waifu_db import waifu_db
 
-test_waifu = Character(249)
-# print(test_waifu.name)
-# print(test_waifu.kanji)
-# print(test_waifu.description)
-# print(test_waifu.actors)
-# print(test_waifu.animeography)
-# print(test_waifu.mangaography)
-print(test_waifu.images)
-# print(test_waifu.initials)
-# print(test_waifu.first)
-# print(test_waifu.last)
-
-# print(waifu_db.keys())
+test_waifu = Character(11)
+print(test_waifu.name.strip())
+print(test_waifu.nicknames)
+print(test_waifu.database_name)
