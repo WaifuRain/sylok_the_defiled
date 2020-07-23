86f5ded69087eedc0eee7a6aaf82fe8058264d05
WaifuRain <Xcalibur996@gmail.com>
2020-07-23 11:55:40 -0400

    test file

--- test.py
+++ test.py
@@ -4,9 +4,23 @@
 # print(test_waifu.first)
 # print(test_waifu.description)
 
-from MAL_Parser import Character
+# from MAL_Parser import Character
+#
+# test_waifu = Character(11)
+# print(test_waifu.name.strip())
+# print(test_waifu.nicknames)
+# print(test_waifu.database_name)
 
-test_waifu = Character(11)
-print(test_waifu.name.strip())
-print(test_waifu.nicknames)
-print(test_waifu.database_name)
+import os
+import random
+waifu_database_root = 'E:\\Waifu Database'
+
+
+root, waifu = next(os.walk(waifu_database_root))[0], random.choice(next(os.walk(waifu_database_root))[1])
+print(waifu)
+print(f'{root}\\{waifu}\\info')
+print(f'{root}\\{waifu}\\actors')
+print(f'{root}\\{waifu}\\appearances\\anime')
+print(f'{root}\\{waifu}\\appearances\\manga')
+print(f'{root}\\{waifu}\\images')
+print(f'{root}\\{waifu}\\nicknames')
